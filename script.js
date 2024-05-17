cf7fdc325694270248d8c9b6e8b99fb3e388d578
DhutiD <53644397+DhutiD@users.noreply.github.com>
2024-05-17 10:36:48 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//let name="Murendeni" 
+//console.log(name)
+//name="Mulalo"
+//console.log(name)  
+
+//let firstName = "Murendeni" //String
+
+//let age = 12 //Number
+
+//let studentsNames = ["Murendeni", "Mulalo"] //Array
+
+//let personInfo = {
+    //"name" : "Murendeni",
+    //"age" : 12
+//} //JSON Objects
+
+let h1Element = document.getElementsByTagName("h1")
+console.log(h1Element)
+
+
+//let plElement = 
